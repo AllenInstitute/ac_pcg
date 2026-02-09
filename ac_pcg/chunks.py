18e7866690e88a64da465c1ee17e4523c3bdda46
Russel Torres <russtorres@gmail.com>
2026-02-09 12:52:54 -0800

    chunks: add some chunk labeling utilities

--- ac_pcg/chunks.py
+++ ac_pcg/chunks.py
@@ -1,5 +1,7 @@
-import cloudvolume
 import dataclasses
+
+import cloudvolume
+import numpy
 
 
 @dataclasses.dataclass
@@ -23,3 +25,37 @@ def iterate_chunk_slice_boxes(arr_shape, chunk_size):
                     bbox=chunk_contains_bb,
                     chunk_idx=(chunk_x, chunk_y, chunk_z)
                 )
+
+
+def chunk_idx_to_bbox(chunk_idx, chunk_size, chunked_box_shape):
+    chunk_mins = tuple(
+        idx * chunk_d for idx, chunk_d in zip(chunk_idx, chunk_size)
+    )
+    chunk_max = tuple(
+        min(box_d, chunk_min + chunk_d) for chunk_min, chunk_d, box_d in zip(
+            chunk_mins, chunk_size, chunked_box_shape)
+    )
+    bbox = cloudvolume.Bbox(chunk_mins, chunk_max)
+    return ChunkBbox(
+        bbox=bbox,
+        chunk_idx=chunk_idx
+    )
+
+
+def get_bbox_chunks(bbox, chunk_size, offset=None):
+    if offset is not None:
+        raise NotImplementedError
+    (chunk_min, chunk_max), (remainder_min, remainder_max) = numpy.divmod(
+        numpy.array([bbox.minpt, bbox.maxpt]), chunk_size)
+    # chunk_max += remainder_max.astype(bool)
+
+    # TODO could be more clever about dims
+    imin, jmin, kmin = chunk_min
+    imax, jmax, kmax = chunk_max
+    chunks = numpy.mgrid[
+        imin:imax + 1:1,
+        jmin:jmax + 1:1,
+        kmin:kmax + 1:1
+    ].reshape(3, -1).T
+
+    return chunks
